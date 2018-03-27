9cbf08d18aa3e5e419b6bcf1ac353953d367f64c
Jude <julie_wang1@brown.edu>
2018-03-27 11:43:32 -0400

    setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import choices from './data.json'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.choicesLeft = choices.slice();
+    const choice = this.getNewChoice();
+    this.state = {
+      highscore: 0,
+      score : 0,
+      choice: choice,
+      gameOver: false,
+      label: "Does Emirates fly here?",
+    };
+  }
+
+  render() {
+    return (
+      <div>Hello World!</div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
